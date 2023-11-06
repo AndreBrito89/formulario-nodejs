ebab00c63733cda64d4e5ec3673d679c4bd168b4
André <andre.brito.arnaud@gmail.com>
2023-11-06 18:24:48 -0300

    script para criação de tabelas

---
+++ create-table.js
@@ -0,0 +1,23 @@
+import {sql} from './db.js'
+
+
+sql `
+CREATE TABLE IF NOT EXISTS animais (
+    id      TEXT PRIMARY KEY,
+    name   TEXT,
+    raca     TEXT, 
+    idade    INTEGER,
+    user_id TEXT, -- Chave estrangeira para a tabela "users"
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+
+`.then(() => {
+    console.log('tabela videos criada.')
+})
+// CREATE TABLE IF NOT EXISTS users (
+//     id      TEXT PRIMARY KEY,
+//     name TEXT,
+//     email TEXT,
+//     password TEXT
+// );
