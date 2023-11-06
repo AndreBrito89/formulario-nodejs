e09a0b2430acf51331cdfa8fb7a171feedcb982a
André <andre.brito.arnaud@gmail.com>
2023-11-06 19:14:37 -0300

    adiciona cors ao projeto

--- server.js
+++ server.js
@@ -4,26 +4,45 @@ import {
     listUsersController,
     updateUserController,
     deleteUserController,
-  } from './src/controllers/userController.js';
+} from './src/controllers/userController.js';
 
 
-  
-  const server = fastify();
-  
-  // POST
-  server.post('/users', createUserController);
-  
-  // GET
-  server.get('/users', listUsersController);
-  
-  // PUT
-  server.put('/users/:id', updateUserController);
-  
-  // DELETE
-  server.delete('/users/:id', deleteUserController);
+
+const server = fastify();
+
+server.addHook('onRequest', (request, reply, done) => {
+    reply.header('Access-Control-Allow-Origin', '*');
+    reply.header(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PUT, DELETE, OPTIONS'
+    );
+    reply.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept'
+    );
+
+    if (request.method === 'OPTIONS') {
+        reply.send();
+    } else {
+        done();
+    }
+});
+
+
+// POST
+server.post('/users', createUserController);
+
+// GET
+server.get('/users', listUsersController);
+
+// PUT
+server.put('/users/:id', updateUserController);
+
+// DELETE
+server.delete('/users/:id', deleteUserController);
 
 server.listen({
-  port: 3333
-}).then(()=>{
+    port: 3333
+}).then(() => {
     console.log('listening to port 3333');
 });
